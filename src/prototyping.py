9d0f40520ca48800b8ce9d1139c676b1079b065b
SiddhantDeshmukh <sd486@exeter.ac.uk>
2021-09-07 14:12:19 +0200

    Manual grid creation and plotting with neighbour calculations

--- src/prototyping.py
+++ src/prototyping.py
@@ -1,5 +1,8 @@
 # %%
 # Currently just 2D
+from random import randint
+from networkx.generators.community import random_partition_graph
+import numpy as np
 from typing import Tuple
 import networkx as nx
 from networkx.drawing.nx_pydot import to_pydot
@@ -27,6 +30,8 @@ Y_GRID_CELL_SIZE = 1
 # Rooms most often connected by corridors
 # Corridors connected to other corridors mean a change in direction
 
+
+# Have floor tiles, wall tiles, door tiles, etc
 
 class Tile:
   def __init__(self, position: Tuple[int, int],
@@ -82,3 +87,57 @@ rectangle = RectangluarRoom(20, 10)
 to_pydot(square.graph).write_png('../out/square.png')
 to_pydot(rectangle.graph).write_png('../out/rectangle.png')
 # to_pydot(dungeon).write_png('../out/dungeon.png')
+
+# %%
+# Let's do it without networkx since I could just code it all myself
+# 20 x 10 rectangular room coords
+all_nodes = [[x, y] for x, y in product(range(20), range(10))]
+
+
+# Function to find NESW neighbours of a given cell
+def get_neighbours(node):
+  directions = {
+      "N": [0, 1],
+      "E": [1, 0],
+      "S": [0, -1],
+      "W": [-1, 0],
+      # "SW": [-1, -1],
+      # "NW": [-1, 1],
+      # "SE": [1, -1],
+      # "NE": [1, 1]
+  }
+
+  result = []
+  for d in directions.values():
+    neighbour = [node[0] + d[0], node[1] + d[1]]
+    if neighbour in all_nodes:  # checking outer scope here!
+      result.append(neighbour)
+
+  return result
+
+
+# Plot
+fig, axes = plt.subplots(1, 2)
+
+cols = list(zip(*all_nodes))
+x_grid, y_grid = cols[0], cols[1]
+
+# Initial condition
+axes[0].plot(x_grid, y_grid, 'r.')
+initial_node = all_nodes[0]
+axes[0].plot(initial_node[0], initial_node[1], 'bx')
+neighbours = get_neighbours(initial_node)
+for node in neighbours:
+  axes[0].plot(node[0], node[1], 'gs')
+
+# Pick a different node (random)
+axes[1].plot(x_grid, y_grid, 'r.')
+new_node = all_nodes[randint(0, len(all_nodes) - 1)]
+axes[1].plot(new_node[0], new_node[1], 'bx')
+neighbours = get_neighbours(new_node)
+for node in neighbours:
+  axes[1].plot(node[0], node[1], 'gs')
+
+plt.show()
+
+# %%
